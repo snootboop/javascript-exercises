874f3f23e2e266e59c82d5d3d69c4300233a18af
Tyler Gatlin <tyler@tylergatlin.com>
2020-07-22 13:56:17 -0700

    Finish find oldest

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-let findTheOldest = function() {
+let findTheOldest = function(peopleArray) {
+    
+    const todaysDate = new Date();
+    const currentYear = Number(todaysDate.getFullYear());
 
+    const ageArray = [];
+
+    for (i=0; i<peopleArray.length; i++) {
+        if (peopleArray[i].yearOfDeath == undefined) {
+            ageArray[i] = currentYear - peopleArray[i].yearOfBirth;
+        } else {
+            ageArray[i] = peopleArray[i].yearOfDeath - peopleArray[i].yearOfBirth;
+        }
+        
+    }
+    
+    const maxAge = Math.max(...ageArray);
+
+    const isMaxAge = (element) => element == maxAge;
+    
+    maxAgeIndex = ageArray.findIndex( isMaxAge );
+
+    return peopleArray[maxAgeIndex];
 }
 
 module.exports = findTheOldest
