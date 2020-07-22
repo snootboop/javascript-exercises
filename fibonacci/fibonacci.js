91bb816bc85dad14832a97053bf554b273a5acc5
Tyler Gatlin <tyler@tylergatlin.com>
2020-07-22 11:48:19 -0700

    Finish fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let fibNum = [1,1];
+    if (num > 0) {
+        for (i=2; i<num; i++) {
+            fibNum[i] = fibNum[i-1] + fibNum[i-2];
+        }
+        return fibNum[num-1];
+    } else {
+        return "OOPS";
+    }
 }
 
 module.exports = fibonacci
